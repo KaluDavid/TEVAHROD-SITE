b6d5793f2c5d9923d33bf86b971a027607e9a01c
Kalu David <kaludavidinyang@gmail.com>
2024-02-12 18:16:59 +0100

    some changes

--- script.js
+++ script.js
@@ -1,20 +1,9 @@
 
-const myVideo = document.getElementById('siteVideo');
-
-function playVid() {
-    myVideo.play();
-}
-
-function pauseVid() {
-    myVideo.pause();
-}
-
-
 function hamburgerMenu() {
     let x = document.getElementById("visible");
     if (x.style.display === "block") {
         x.style.display = "none";
-    }else{
+    } else {
         x.style.display = "block";
     }
 }
